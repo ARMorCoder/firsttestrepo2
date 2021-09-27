f7a849a3cacad23064a177cfd5880f5381a43cde
Anthony Martinez <armmartinez158@gmail.com>
2021-09-27 14:24:09 -0500

    This is my second commit

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello");
 		System.out.println("How are you");
+		System.out.println("THIS IS A SCP FOUNDATION");
 
 	}
 
